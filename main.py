74b5d0565b254846e3ce1399675ff6430aad32db
Mandla Mbuli <lm.mbuli@gmail.com>
2015-01-03 14:04:15 +0200

    added grid to the window

--- main.py
+++ main.py
@@ -4,27 +4,40 @@ kivy.require('1.8.0')
 from kivy.app import App
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.properties import ObjectProperty
+from kivy.properties import BooleanProperty
 from kivy.uix.button import Button
 
 from generate import Grid
 
 
-class MainView(AnchorLayout):
+class Tile(Button):
+    selected = BooleanProperty()
+
+    def on_touch_move(self, touch):
+        if self.collide_point(*touch.pos):
+            self.selected = True
+
+
+class GridView(AnchorLayout):
     grid = ObjectProperty()
+
+    def __init__(self, data, **kwargs):
+        super(GridView, self).__init__(**kwargs)
+        self.reset_grid(data)
+        self.selected = []
 
     def reset_grid(self, data):
         self.grid.clear_widgets()
         self.grid.rows = len(data)
         self.grid.cols = len(data[0])
 
-        for row in range(0, self.grid.rows):
-            for col in range(0, self.grid.cols):
-                self.grid.add_widget(Button(text=str(data[row][col])))
+        for row in data:
+            for letter in row:
+                self.grid.add_widget(Tile(text=str(letter)))
 
 
 class Funamagama(App):
     def build(self):
-        main_view = MainView()
         grid = Grid(20, 20)
         grid.place(*['leonard',
                      'mandla',
@@ -42,8 +55,8 @@ class Funamagama(App):
                      'perfect',
                      'nothing',
                      'coven'])
-        main_view.reset_grid(grid._grid)
-        return main_view
+        mainview = GridView(grid._grid)
+        return mainview
 
 
 if __name__ == "__main__":

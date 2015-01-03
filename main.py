06630bfe07011ff99e83b90eac26870c9fb7d124
Mandla Mbuli <lm.mbuli@gmail.com>
2015-01-03 18:13:51 +0200

    I can draw a straight line now between two points

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.properties import ObjectProperty
 from kivy.uix.button import Button
+from kivy.graphics import Color, Ellipse, Line
+
+from random import random
 
 from generate import Grid
 
@@ -29,11 +32,21 @@ class GridView(GridLayout):
                 self.add_widget(Tile(text=str(letter)))
 
     def on_touch_down(self, touch):
-        self.selected = []
+        color = (random(), 1, 1)
+        with self.canvas:
+            Color(*color, mode='hsv')
+            d = 10.
+            Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=(d, d))
+            self.start = [touch.x, touch.y]
+            touch.ud['line'] = Line(points=self.start)
+
+    def on_touch_move(self, touch):
+        touch.ud['line'].points = self.start + [touch.x, touch.y]
 
     def on_touch_up(self, touch):
-        for button in set(self.selected):
-            print(button.text)
+        with self.canvas:
+            d = 10.
+            Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=(d, d))
 
 
 class MainView(AnchorLayout):
